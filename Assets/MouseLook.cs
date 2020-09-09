c28a77f4390c742f0aa36a23c09d74f1301f63e6
Matt Barnfield <matt.barnfield4@gmail.com>
2020-09-09 22:15:07 +0100

    Add prototype third-person character control

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float sensitivity = 100;
+    public float viewClamping = 40f;
+
+
+    private float xRotation = 0f;
+    private float yRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        xRotation -=  Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+        yRotation += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+
+        xRotation = Mathf.Clamp(xRotation, -viewClamping, viewClamping);
+        yRotation = Mathf.Clamp(yRotation, -viewClamping, viewClamping);
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 12f;
+    private CharacterController controller;
+
+    public float gravity = 9.81f;
+    public float jumpStrength = 20f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded)
+        {
+            if (velocity.y < 0) {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetKeyDown("space"))
+            {
+                velocity.y = jumpStrength;
+            }
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        velocity = transform.right * x * speed +
+            transform.up * (velocity.y + -gravity * Time.deltaTime) +
+            transform.forward * z * speed;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
